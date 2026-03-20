92e6dc3bb3ad2ad2a414bc596df1716e13cbf4e2
Bamo0507 <66499599+Bamo0507@users.noreply.github.com>
2026-03-19 20:10:07 -0600

    estructura de proyecto

---
+++ searchAlgorithms/models/node.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass, field
+from typing import Any
+
+
+@dataclass
+class Node:
+    state: Any
+    parent: "Node | None" = field(default=None)  # si es raiz, no hay padre
+    action: Any = field(default=None) # la accion que se tomo para llegar a este estado
+    path_cost: float = field(default=0.0) # el costo acumulado desde la raiz hasta este nodo
---
+++ searchAlgorithms/models/queueStructures.py
@@ -0,0 +1,57 @@
+class LIFO:
+    def __init__(self):
+        self.queue = []
+
+    def empty(self):
+        return len(self.queue) == 0
+
+    def top(self):
+        return self.queue[0]
+
+    def pop(self):
+        return self.queue.pop(0)
+
+    def add(self, element):
+        self.queue.insert(0, element)
+        return self.queue
+
+
+class FIFO:
+    def __init__(self):
+        self.queue = []
+
+    def empty(self):
+        return len(self.queue) == 0
+
+    def top(self):
+        return self.queue[0]
+
+    def pop(self):
+        return self.queue.pop(0)
+
+    def add(self, element):
+        self.queue.append(element)
+        return self.queue
+
+
+class Priority:
+    def __init__(self):
+        self.queue = []  # lista de tuplas (element, priority)
+
+    def empty(self):
+        return len(self.queue) == 0
+
+    def top(self):
+        return self.queue[0]
+
+    def pop(self):
+        return self.queue.pop(0)
+
+    def add(self, element, priority):
+        position = len(self.queue)  # por defecto va al final
+        for i in range(len(self.queue)):
+            if priority > self.queue[i][1]:
+                position = i
+                break
+        self.queue.insert(position, (element, priority))
+        return self.queue
